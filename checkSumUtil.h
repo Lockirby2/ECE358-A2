0e42460fa823af1ecc6d343f4fa1b71b64c029f6
Cole Gerard Vander Veen <cgvander@ecelinux2.uwaterloo.ca>
2017-07-25 14:30:15 -0400

    Forgot to change header

--- checkSumUtil.h
+++ checkSumUtil.h
@@ -5,5 +5,5 @@ using namespace std;
 class CheckSumUtil{
 public:
     uint16_t static computeSum(void* bytestream, int streamSize);
-    bool static checkChecksum(int16_t checksum, void* bytestream, int streamSize);
+    bool static checkChecksum(uint16_t checksum, void* bytestream, int streamSize);
 };
